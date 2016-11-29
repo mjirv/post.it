f1d996b815885c981e1cb3aae0386ad9f99ebc33
Michael Irvine <michael.j.irvine@gmail.com>
2016-11-28 22:37:59 -0800

    Updating more for wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
