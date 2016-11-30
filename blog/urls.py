187fbe17e74bc525e2303a94e52fe7871a0c38f5
Michael Irvine <michael.j.irvine@gmail.com>
2016-11-29 20:49:50 -0800

    o noes 404

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^post/(?P<post_id>[0-9]+)$', views.detail, name='detail'),
-	url(r'^static/(?P<page_id>[0-9]+)$', views.static, name='static')
+	url(r'^$', blog.views.index, name='index'),
+	url(r'^post/(?P<post_id>[0-9]+)$', blog.views.detail, name='detail'),
+	url(r'^static/(?P<page_id>[0-9]+)$', blogviews.static, name='static')
 ]
