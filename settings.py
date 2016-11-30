a96863306bc933d15d140e2d9ee3e9e2735cc542
Michael Irvine <michael.j.irvine@gmail.com>
2016-11-29 21:19:24 -0800

    need the models

--- settings.py
+++ settings.py
@@ -24,7 +24,15 @@ DATABASES = {
 
 ROOT_URLCONF = 'blog.urls'
 
-INSTALLED_APPS = ['blog', 'el_pagination']
+INSTALLED_APPS = ['blog',
+	'el_pagination',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles'
+	]
 
 LOGGING = {
     'version': 1,
